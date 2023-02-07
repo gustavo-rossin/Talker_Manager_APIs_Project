f8b79111a6bc2117acfdc0635c1196f2e5f1f220
gustavo-rossin <gustavo.rossin@hotmail.com>
2023-02-06 21:47:17 -0300

    feat: req 05 done - ordem dos IFs alterou a query

--- src/middlewares/validateRate.js
+++ src/middlewares/validateRate.js
@@ -2,15 +2,13 @@ const validateRate = (req, res, next) => {
   const { talk } = req.body;
   const ratings = [1, 2, 3, 4, 5];
 
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
   if (!talk.rate) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
-
-  if (+talk.rate < 1 || +talk.rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-    }
-
-  if (!+talk.rate in ratings) {
+  if (!(talk.rate in ratings)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
 
